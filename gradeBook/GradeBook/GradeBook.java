e6ee88d1858440104f312b8f271def11bf9c974e
Andrew <kprtexas@gmail.com>
2015-05-29 18:56:46 -0500

    Basic contentPane added

--- gradeBook/GradeBook/GradeBook.java
+++ gradeBook/GradeBook/GradeBook.java
@@ -1,13 +1,22 @@
 package GradeBook;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class GradeBook extends JFrame 
 {
+	private Container contentPane;
 	
 	public GradeBook() 
 	{
 		// TODO Auto-generated constructor stub
+		contentPane = getContentPane();
+		contentPane.setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("GradeBook");
+		setSize(700,700);
+		setLocationRelativeTo(null);
+		setResizable(false);
 		
 	}
 	public static void main(String args[])
