038e0b34b859b3d262f3311804b056d1c005ec36
Andrew <kprtexas@gmail.com>
2015-05-28 21:18:13 -0500

    Basic Administrator

--- gradeBook/GradeBook/GradeBook.java
+++ gradeBook/GradeBook/GradeBook.java
@@ -1,11 +1,18 @@
 package GradeBook;
 
-public class GradeBook {
+import javax.swing.JFrame;
+
+public class GradeBook extends JFrame {
 
 	public GradeBook() 
 	{
 		// TODO Auto-generated constructor stub
 		Admin admin = new Admin();
+	}
+	public static void main(String args[])
+	{
+		GradeBook frame = new GradeBook();
+		frame.setVisible(true);
 	}
 
 }
