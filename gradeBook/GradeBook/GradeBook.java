ad8f393919b3994a80f596091fc3491802b689f4
Andrew <kprtexas@gmail.com>
2015-05-29 21:30:34 -0500

    update basic login

--- gradeBook/GradeBook/GradeBook.java
+++ gradeBook/GradeBook/GradeBook.java
@@ -1,12 +1,17 @@
 package GradeBook;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class GradeBook extends JFrame 
+public class GradeBook extends JFrame implements ActionListener
 {
 	private Container contentPane;
-	
+	private JTextField username = new JTextField();
+	private JTextField password = new JTextField();
+	private JButton login = new JButton("Login");
 	public GradeBook() 
 	{
 		// TODO Auto-generated constructor stub
@@ -18,6 +23,18 @@ public class GradeBook extends JFrame
 		setLocationRelativeTo(null);
 		setResizable(false);
 		
+		username.setBounds(300,250,100,50);
+		username.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		contentPane.add(username);
+		
+		password.setBounds(300,350,100,50);
+		password.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		contentPane.add(password);
+		
+		login.setBounds(300, 450, 100,50);
+		contentPane.add(login);
+		login.addActionListener(this);
+		
 	}
 	public static void main(String args[])
 	{
@@ -27,5 +44,18 @@ public class GradeBook extends JFrame
 	private boolean checkPassword(String userName, String password)
 	{
 		return false;
+	}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		if(e.getSource() instanceof JButton)
+		{
+			if(e.getSource() == login)
+			{
+				//
+			}
+		}
+		
+		
 	}
 }
