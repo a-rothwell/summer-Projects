17bd06580e3fcdc219528fda13fa34169163fe16
Andrew <kprtexas@gmail.com>
2015-06-09 17:22:31 -0500

    Added new project final project set up renamed packages

---
+++ antSimulator/antSimulator/AntSimulator.java
@@ -0,0 +1,14 @@
+package antSimulator;
+
+public class AntSimulator {
+
+	public AntSimulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
