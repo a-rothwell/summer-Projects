809db3cad57fe6aa92ba0f2fa7498cae0ffb7e6b
Andrew <kprtexas@gmail.com>
2015-07-03 14:32:36 -0500

    Inital Commit of new projet Commerce Removed Unstarted Project Translator

---
+++ commerce/commerce/Commerce.java
@@ -0,0 +1,34 @@
+package commerce;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Andrew
+ * @version Alpha .001A;
+ */
+public class Commerce extends JFrame implements ActionListener
+{
+
+	public Commerce() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Commerce frame = new Commerce();
+		frame.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
