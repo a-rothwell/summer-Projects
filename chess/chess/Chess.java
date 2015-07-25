554c7a106f26fcf7e7768e4f0cb3bd34aabda089
andrew <kprtexas@gmail.com>
2015-07-25 15:28:29 -0500

    Commit for new project

---
+++ chess/chess/Chess.java
@@ -0,0 +1,22 @@
+package chess;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Chess extends JFrame implements ActionListener{
+
+	public Chess() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
