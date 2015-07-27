b17d720507e182ab7b4075d56afe235f538cd99c
andrew <kprtexas@gmail.com>
2015-07-26 22:10:42 -0500

    Board Created

--- chess/chess/Tile.java
+++ chess/chess/Tile.java
@@ -1,16 +1,25 @@
 package chess;
 
+import java.awt.Color;
 import java.awt.Point;
 
 import javax.swing.JButton;
 
 public class Tile extends JButton 
 {
+	private Color backGroundColor;
 	private Point location;
-	
 	public Tile(int xLetter, int yLocation) 
 	{
-		
+		if(yLocation % 2 ==0)
+		{
+			backGroundColor = xLetter % 2 == 0 ?Color.BLACK: Color.WHITE;
+		}
+		else
+		{
+			backGroundColor = xLetter % 2 == 0 ?Color.WHITE: Color.BLACK;
+		}
+		this.setBackground(backGroundColor);
 	}
-
+	
 }
